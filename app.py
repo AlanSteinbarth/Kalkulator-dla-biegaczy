9054e63510ec461e6727d0381dbc2f0c6b6026d7
Wiktoria839 <wiktoria.steinbarth@gmail.com>
2025-05-20 11:22:57 +0200

    problem z kluczem

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
+import os, streamlit as st
+st.write("DEBUG: OPENAI_API_KEY =", os.getenv("OPENAI_API_KEY"))
+
+
 #wczytanie zmiennych 
 #load_dotenv()
 openai_client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
